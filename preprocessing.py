9040c87aaa9eef31d7154104eb1fe99965a4fe05
mdschramm <mark@air.inc>
2023-12-16 15:56:13 -0500

    refactor

--- preprocessing.py
+++ preprocessing.py
@@ -12,20 +12,15 @@ print(NHANES_13_14.shape)
 assert (len(NHANES_13_14.columns) == len(NHANES_15_16.columns))
 assert (len(NHANES_13_14.columns) == len(NHANES_17_18.columns))
 
-refused_values = [7, 77, 777, 7777, 77777, 777777]
-idk_values = [9, 99, 999, 9999, 99999, 999999]
 
-missing_values = ['', np.nan]
-
-
-def preprocess(df):
-    df = df.replace(missing_values, -1)
-
+# Adds indicators for all refused and idk columns
+def missing_indicate_refused_and_idk():
+    refused_values = [7, 77, 777, 7777, 77777, 777777]
+    idk_values = [9, 99, 999, 9999, 99999, 999999]
     refused_magic_num, idk_magic_num = (
         np.random.uniform(), np.random.uniform())
     df = df.replace(refused_values, refused_magic_num)
     df = df.replace(idk_values, idk_magic_num)
-
     refused_idk_encoder = ColumnTransformer(
         [('refused', MissingIndicator(
             missing_values=refused_magic_num, features='all'), df.columns),
@@ -33,9 +28,17 @@ def preprocess(df):
              missing_values=idk_magic_num, features='all'), df.columns)
          ]
     )
+
+
+missing_values = ['', np.nan]
+
+
+def preprocess(df):
+    df = df.replace(missing_values, -1)
+
     union = FeatureUnion([
         ('existing_data', 'passthrough'),
-        ('refused_idk_encoder', refused_idk_encoder),
+        ('refused_idk_encoder', missing_indicate_refused_and_idk()),
     ])
 
     # now need to impute magic numbers with something
