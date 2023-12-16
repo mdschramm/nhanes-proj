6b73f61cf17eb371137e83b04442cd59020395b7
mdschramm <mark@air.inc>
2023-12-16 16:10:28 -0500

    bug fix

--- preprocessing.py
+++ preprocessing.py
@@ -13,37 +13,39 @@ assert (len(NHANES_13_14.columns) == len(NHANES_15_16.columns))
 assert (len(NHANES_13_14.columns) == len(NHANES_17_18.columns))
 
 
-# Adds indicators for all refused and idk columns
-def missing_indicate_refused_and_idk():
+# Adds indicators for all refused, idk, and missing columns
+def missing_indicators(df):
     refused_values = [7, 77, 777, 7777, 77777, 777777]
     idk_values = [9, 99, 999, 9999, 99999, 999999]
-    refused_magic_num, idk_magic_num = (
-        np.random.uniform(), np.random.uniform())
+    refused_magic_num, idk_magic_num, missing_magic_num = (
+        np.random.uniform(), np.random.uniform(), np.random.uniform())
     df = df.replace(refused_values, refused_magic_num)
     df = df.replace(idk_values, idk_magic_num)
-    refused_idk_encoder = ColumnTransformer(
-        [('refused', MissingIndicator(
-            missing_values=refused_magic_num, features='all'), df.columns),
-         ('idk', MissingIndicator(
-             missing_values=idk_magic_num, features='all'), df.columns)
-         ]
+    df = df.replace(np.nan, missing_magic_num)
+
+    return df, ColumnTransformer(
+        [
+            ('missing', MissingIndicator(
+                missing_values=missing_magic_num, features='all'), df.columns),
+            ('refused', MissingIndicator(
+                missing_values=refused_magic_num, features='all'), df.columns),
+            ('idk', MissingIndicator(
+             missing_values=idk_magic_num, features='all'), df.columns),
+
+        ]
     )
 
 
-missing_values = ['', np.nan]
-
-
 def preprocess(df):
-    df = df.replace(missing_values, -1)
-
+    new_df, missing_transformer = missing_indicators(df)
     union = FeatureUnion([
         ('existing_data', 'passthrough'),
-        ('refused_idk_encoder', missing_indicate_refused_and_idk()),
+        ('missing_indicators', missing_transformer),
     ])
 
     # now need to impute magic numbers with something
 
-    return union.fit_transform(df)
+    return union.fit_transform(new_df)
 
 
 processed_data = preprocess(NHANES_13_14)
