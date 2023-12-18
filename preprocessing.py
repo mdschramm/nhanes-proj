931cacab551b6457feb5699aefc6b3e52ceba4c9
mdschramm <mark@air.inc>
2023-12-18 16:42:42 -0500

    remove missing counts above threshold

--- preprocessing.py
+++ preprocessing.py
@@ -16,6 +16,8 @@ REFUSED_MAGIC_NUM, IDK_MAGIC_NUM, MISSING_MAGIC_NUM = (
     np.random.uniform(), np.random.uniform(), np.random.uniform())
 
 # Adds indicators for all refused, idk, and missing columns
+print('REFUSED_MAGIC_NUM', REFUSED_MAGIC_NUM, 'IDK_MAGIC_NUM',
+      IDK_MAGIC_NUM, 'MISSING_MAGIC_NUM', MISSING_MAGIC_NUM)
 
 
 def missing_indicator_transformer(df):
@@ -25,7 +27,6 @@ def missing_indicator_transformer(df):
     df.replace(refused_values, REFUSED_MAGIC_NUM, inplace=True)
     df.replace(idk_values, IDK_MAGIC_NUM, inplace=True)
     df.replace(np.nan, MISSING_MAGIC_NUM, inplace=True)
-    print(df['SEQN'])
 
     return ColumnTransformer(
         [
@@ -39,7 +40,35 @@ def missing_indicator_transformer(df):
     )
 
 
+class ColumnFilterTransfomer():
+    def __init__(self, threshold=.3):
+        self.threshold = threshold
+
+    def transform(self, X, y=None):
+        n_rows, n_cols = X.shape
+        threshold = self.threshold*n_rows
+        remove_cols = []
+        for i in range(n_cols):
+            counts_map = dict(zip(*np.unique(X[:, i], return_counts=True)))
+            missing_counts = counts_map[MISSING_MAGIC_NUM] if MISSING_MAGIC_NUM in counts_map else 0
+            refused_counts = counts_map[REFUSED_MAGIC_NUM] if REFUSED_MAGIC_NUM in counts_map else 0
+            idk_counts = counts_map[IDK_MAGIC_NUM] if IDK_MAGIC_NUM in counts_map else 0
+            n_bad = missing_counts + refused_counts + idk_counts
+            if n_bad > threshold:
+                remove_cols.append(i)
+
+        print('num columns to remove:', len(remove_cols))
+        X = np.delete(X, remove_cols, axis=1)
+        return X
+
+    def fit(self, df, y=None):
+        return self
+
+
 def process_data(df):
+    # replace all strings in dataset
+    df.drop(inplace=True, columns=(df.select_dtypes(
+        exclude=['int64', 'float64']).columns))
     process_pipe = Pipeline(
         [
             # Adds missing indicators for refused, idk, and missing,
@@ -48,13 +77,14 @@ def process_data(df):
                 ('existing_data', 'passthrough'),
                 ('missing_indicators', missing_indicator_transformer(
                     df)),
-            ]))
+            ])),
+
+            ('drop_underfilled_columns', ColumnFilterTransfomer())
         ]
     )
     res = process_pipe.fit_transform(df)
     return res
 
 
-print(NHANES_13_14.shape)
 res = process_data(NHANES_13_14)
 print(res.shape)
