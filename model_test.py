502e5d7c4fa58549c4a3d5fe1a37e6778965f560
mdschramm <mark@air.inc>
2023-12-18 21:29:36 -0500

    testing logistic regression

--- model_test.py
+++ model_test.py
@@ -3,8 +3,8 @@ from preprocessing import process_data
 from dataload import NHANES_13_14, select_features
 from sklearn.model_selection import StratifiedShuffleSplit
 
-from sklearn.linear_model import Lasso
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import Lasso, LogisticRegression
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 
 
 def drop_bad_y_rows(df, y_col, bad_values):
@@ -19,7 +19,7 @@ data = drop_bad_y_rows(data, y_col,
                        bad_values=[7, 9, np.nan])
 
 X_raw = data[list(features)]
-y = data[y_col].to_numpy()
+y = np.round(data[y_col].to_numpy()).astype(int)
 X = process_data(X_raw)
 splitter = StratifiedShuffleSplit(n_splits=1, test_size=.2, random_state=123)
 
@@ -30,19 +30,27 @@ y_train = y[train_idx]
 X_test = X[test_idx]
 y_test = y[test_idx]
 
-# Mean Squared Error: 0.9227709616185261
-# R^2 Score: -0.0007543302727242018
 
-
-def test_lasso():
-    lasso = Lasso(fit_intercept=True)
-    lasso.fit(X_train, y_train)
-    y_pred = lasso.predict(X_test)
+def test_model(model, params):
+    clf = model(random_state=123, **params)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
-    print(y_pred, y_test)
+    acc = accuracy_score(y_test, y_pred)
     print(f'Mean Squared Error: {mse}')
     print(f'R^2 Score: {r2}')
+    print(f'accuracy: {acc}')
 
 
-test_lasso()
+# Mean Squared Error: 0.9227709616185261
+# R^2 Score: -0.0007543302727242018
+# params = {'fit_intercept': True, }
+# test_model(Lasso, params)
+
+
+# Mean Squared Error: 1.3030583873957369
+# R^2 Score: -0.41317984421313847
+# accuracy: 0.6329935125115848
+params = {}
+test_model(LogisticRegression, params)
